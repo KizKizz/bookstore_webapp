454d9f631282c225a3b74b4110c9dd0dda977355
KizKizz <xkizkizz@gmail.com>
2022-04-04 10:03:38 -0700

    fixes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,9 +26,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "9d216bb1e977e95ae1feeaf4909c9a08",
-"/": "9d216bb1e977e95ae1feeaf4909c9a08",
-"main.dart.js": "75d924d5e29371e8f55eb7768224f79a",
+"index.html": "c491c5430dca45f3ca40e3dc6df5785b",
+"/": "c491c5430dca45f3ca40e3dc6df5785b",
+"main.dart.js": "11636f10e6a4bd512b718a00e8ed2891",
 "manifest.json": "a2016f99084e6571003b54b24ce90945",
 "version.json": "5744065190e3748f155b1400e434dc82"
 };
